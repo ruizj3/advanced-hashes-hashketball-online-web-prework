1d817c946c05cfdb7388be1db4de27bf20d18389
cool-changelog-3932 <ruizj3@uw.edu>
2019-10-16 02:31:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -140,3 +140,19 @@ def team_names
     team[:team_name]
   end
 end
+
+def player_numbers(team_name)
+  nums = []
+  game_hash.each do |_place, team|
+    next unless team[:team_name] == team_name
+
+    team.each do |attribute, data|
+      next unless attribute == :players
+
+      data.each do |data|
+        nums << data[:number]
+      end
+    end
+  end
+  nums
+end
