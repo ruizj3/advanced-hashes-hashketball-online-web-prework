eddf4cbe40c695ba1b4914f38a3a0e81f3a036e7
cool-changelog-3932 <ruizj3@uw.edu>
2019-10-16 01:57:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,10 @@
 # Write your code here!
-
-
-
-
-
-
-
-
-
-
+def game_hash
+  {
+    home:{team_name:'Brooklyn Nets',
+          colors:['Black', 'White'],
+          players:},
+    away:{team_name:'Charlotte Hornets',
+          colors:['Turquoise', 'Purple'],
+          players:}
+  }
